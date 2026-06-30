0d118f8372e34813b0a4b464ed01a3cde38b8742
thaylasampaio-ui <thayla.sampaio@escola.pr.gov.br>
2026-06-29 22:28:09 -0300

    agrinho2026

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 var tamanho = 20;
 
+function aumentar(){
+    tamanho += 2;
+    document.body.style.fontSize = tamanho + "px";
+}
+
+function diminuir(){
+    tamanho -= 2;
+    document.body.style.fontSize = tamanho + "px";
+}
+
 function escuro(){
-    document.body.style.backgroundColor="black";
-    document.body.style.color="white";
+    document.body.style.backgroundColor = "#111";
+    document.body.style.color = "white";
 }
 
 function claro(){
-    document.body.style.backgroundColor="white";
-    document.body.style.color="black";
+    document.body.style.backgroundColor = "#f4f4f4";
+    document.body.style.color = "#222";
 }
 
 function mostrar(){
 
 var nome = document.getElementById("nome").value;
-
 var idade = document.getElementById("idade").value;
 
 document.getElementById("resposta").innerHTML =
-"Olá " + nome + "! Você tem " + idade + " anos.";
+"Olá " + nome + ", você tem " + idade + " anos.";
 
 }
