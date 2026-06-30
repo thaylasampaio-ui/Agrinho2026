dc561b866650e9414801a21e82c0ad181be7b407
thaylasampaio-ui <thayla.sampaio@escola.pr.gov.br>
2026-06-29 21:46:39 -0300

    agrinho2026

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 var tamanho = 20;
 
-function aumentar(){
-    tamanho = tamanho + 2;
-    document.body.style.fontSize = tamanho + "px";
-}
-
-function diminuir(){
-    tamanho = tamanho - 2;
-    document.body.style.fontSize = tamanho + "px";
-}
-
 function escuro(){
     document.body.style.backgroundColor="black";
     document.body.style.color="white";
